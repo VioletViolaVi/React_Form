2078723529cb5af5680766f91bcda6dd4f0b3b12
vivian <vivian.v18723@gmail.com>
2022-03-27 16:16:37 +0100

    Set state and create functions for handling data.

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,26 +1,84 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  // set state
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    subscribe: false,
+  });
+
+  // handles data entering input
+  const handleData = (event) => {
+    setFormData((prevFormData) => {
+      const { name, value, type, checked } = event.target;
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
+
+  // for submitting form
+  const submitForm = (event) => {
+    event.preventDefault();
+    // shows data
+    console.log(formData);
+    // message for subscribing
+    formData.subscribe &&
+      console.log(
+        `Thanks for subscribing ${
+          formData.firstName.charAt(0).toUpperCase() +
+          formData.firstName.slice(1)
+        }!`
+      );
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={submitForm}>
         <input
+          required
           type="text"
           id="firstName"
           name="firstName"
-          placeholder="firstName"
+          value={formData.firstName}
+          onChange={handleData}
+          placeholder="First Name"
         />
         <br />
 
         <input
+          required
           type="text"
           id="lastName"
           name="lastName"
-          placeholder="lastName"
+          value={formData.lastName}
+          onChange={handleData}
+          placeholder="Last Name"
         />
         <br />
 
-        <input type="checkbox" id="subscribe" name="subscribe" />
+        <input
+          required
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleData}
+          placeholder="Email"
+        />
+        <br />
+
+        <input
+          type="checkbox"
+          id="subscribe"
+          name="subscribe"
+          checked={formData.subscribe}
+          onChange={handleData}
+        />
         <br />
 
         <button>Submit</button>
